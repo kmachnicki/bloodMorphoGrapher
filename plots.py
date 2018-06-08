e9e6220255baf102c53817295d13ab11e24535fa
kmachnicki <kamil0602@gmail.com>
2018-06-08 09:57:40 +0200

    Data normal values drawing for women

--- plots.py
+++ plots.py
@@ -14,42 +14,60 @@ rcParams["text.latex.unicode"] = True
 plt.style.use("classic")
 
 class Probe(object):
-    def __init__(self, data_name, data_unit, data_limits, data_description):
-        self.data_name = data_name
-        self.data_unit = data_unit
-        self.data_limits = data_limits
-        self.data_description = data_description
-        self.data_samples = []
+    def __init__(self, name, unit, limits_men, limits_women, description):
+        self.name = name
+        self.unit = unit
+        self.limits_men = limits_men
+        self.limits_women = limits_women
+        self.description = description
+        self.samples = []
 
-    def add_data_sample(self, data_sample):
-        self.data_samples.append(data_sample)
+    def get_name(self):
+        return self.name
+
+    def get_unit(self):
+        return self.unit
+
+    def get_limits(self, for_women):
+        if for_women is True:
+            return self.limits_women
+        return self.limits_men
+
+    def get_limits_men(self):
+        return self.limits_men
+
+    def get_samples(self):
+        return self.samples
+
+    def add_sample(self, sample):
+        self.samples.append(sample)
 
     def __str__(self):
-        return u"%s - %s" % (self.data_name, self.data_description)
+        return u"%s - %s" % (self.name, self.description)
 
     def __repr__(self):
-        return u"%s: %s-%s %s - %s" % (self.data_name, str(self.data_limits[0]), str(self.data_limits[1]),
-            self.data_unit, self.data_description)
+        return u"%s: %s-%s %s - %s" % (self.name, str(self.limits[0]), str(self.limits[1]), self.unit, self.description)
 
     def __eq__(self, other):
-        return self.data_name == other.data_name
+        return self.name == other.name
 
 class Morphology(object):
-    def __init__(self):
+    def __init__(self, data_limits_for_women):
+        self.data_limits_for_women = data_limits_for_women
         self.dates = []
         self.probes = {
-            "RBC": Probe("RBC", "[mln/µl]", [4.5, 5.9], u"Ilość czerwonych krwinek (erytrocytów)"),
-            "HGB": Probe("HGB", "[g/dl]", [13.5, 18.5], u"Ilość hemoglobiny w ogólnej masie erytrocytów"),
-            "HCT": Probe("HCT", "[%]", [40, 54], u"Hematokryt - procentowa objętość erytrocytów we krwi"),
-            "MCV": Probe("MCV", "[fl]", [76, 102.9], u"Średnia objętość erytrocytu"),
-            "MCH": Probe("MCH", "[pg]", [25.7, 35.7], u"Stężenie hemoglobiny w erytrocycie"),
-            "MCHC": Probe("MCHC", "[g/dl]", [29.5, 38.8], u"Stężenie hemoglobiny w ogólnej masie erytrocytów"),
-            "RDW": Probe("RDW", "[%]", [10.3, 15.9], u"Anizocytoza erytrocytów - rozpiętość rozkładu objętości erytrocytów"),
-            "PLT": Probe("PLT", "[tys/µl]", [135, 495], u"Ilość płytek krwi (trombocytów)"),
-            "PDW": Probe("PDW", "[%]", [11, 18], u"Anizocytoza trombocytów - rozpiętość rozkładu objętości trombocytów"),
-            "MPV": Probe("MPV", "[fl]", [6, 11], u"Średnia objętość trombocytu"),
-            "WBC": Probe("WBC", "[tys/µl]", [3.8, 10.5], u"Ilość białych krwinek (leukocytów)"),
-            "PCT": Probe("PCT", "[%]", [0.2, 0.5], u"Poziom prokalcytoniny (białka tarczycy) w osoczu krwi"),
+            "RBC": Probe("RBC", "[mln/µl]", [4.5, 5.9], [4.2, 5.4], u"Ilość czerwonych krwinek (erytrocytów)"),
+            "HGB": Probe("HGB", "[g/dl]", [13.5, 18.5], [12, 16], u"Ilość hemoglobiny w ogólnej masie erytrocytów"),
+            "HCT": Probe("HCT", "[%]", [40, 54], [40, 51], u"Hematokryt - procentowa objętość erytrocytów we krwi"),
+            "MCV": Probe("MCV", "[fl]", [76, 102.9], [76, 102.9], u"Średnia objętość erytrocytu"),
+            "MCH": Probe("MCH", "[pg]", [25.7, 35.7], [25.7, 35.7], u"Stężenie hemoglobiny w erytrocycie"),
+            "MCHC": Probe("MCHC", "[g/dl]", [29.5, 38.8], [29.5, 38.8], u"Stężenie hemoglobiny w ogólnej masie erytrocytów"),
+            "RDW": Probe("RDW", "[%]", [10.3, 15.9], [10.3, 15.9], u"Anizocytoza erytrocytów - rozpiętość rozkładu objętości erytrocytów"),
+            "PLT": Probe("PLT", "[tys/µl]", [135, 495], [135, 495], u"Ilość płytek krwi (trombocytów)"),
+            "PDW": Probe("PDW", "[%]", [11, 18], [11, 18], u"Anizocytoza trombocytów - rozpiętość rozkładu objętości trombocytów"),
+            "MPV": Probe("MPV", "[fl]", [6, 11], [6, 11], u"Średnia objętość trombocytu"),
+            "WBC": Probe("WBC", "[tys/µl]", [3.8, 10.5], [3.8, 10.5], u"Ilość białych krwinek (leukocytów)"),
+            "PCT": Probe("PCT", "[%]", [0.2, 0.5], [0.2, 0.5], u"Poziom prokalcytoniny (białka tarczycy) w osoczu krwi"),
             }
 
     def get_probe(self, probe_name):
@@ -58,8 +76,11 @@ class Morphology(object):
     def get_probes_size(self):
         probes_size = []
         for probe in self.probes.values():
-            probes_size.append(len(probe.data_samples))
+            probes_size.append(len(probe.get_samples()))
         return probes_size
+        
+    def show_data_limits_for_women(self):
+        return self.data_limits_for_women
 
     def is_data_complete(self):
         return all(probe_size == len(self.dates) for probe_size in self.get_probes_size())
@@ -89,7 +110,7 @@ class Morphology(object):
                         if key == "date":
                             self.dates.append(val)
                         elif key in self.probes:
-                            self.probes.get(key).add_data_sample(float(val))
+                            self.probes.get(key).add_sample(float(val))
             if self.is_data_empty():
                 self.empty_data_error(filename)
             if not self.is_data_complete():
@@ -136,9 +157,11 @@ def draw_first_part_of_erythrocytes(morphology, show_only):
     HGB = morphology.get_probe("HGB")
     HCT = morphology.get_probe("HCT")
 
-    draw(ax1, RBC.data_samples, RBC.data_limits, RBC.data_name, RBC.data_unit, morphology.dates)
-    draw(ax2, HGB.data_samples, HGB.data_limits, HGB.data_name, HGB.data_unit, morphology.dates)
-    draw(ax3, HCT.data_samples, HCT.data_limits, HCT.data_name, HCT.data_unit, morphology.dates)
+    for_women = morphology.show_data_limits_for_women()
+    
+    draw(ax1, RBC.get_samples(), RBC.get_limits(for_women), RBC.get_name(), RBC.get_unit(), morphology.dates)
+    draw(ax2, HGB.get_samples(), HGB.get_limits(for_women), HGB.get_name(), HGB.get_unit(), morphology.dates)
+    draw(ax3, HCT.get_samples(), HCT.get_limits(for_women), HCT.get_name(), HCT.get_unit(), morphology.dates)
 
     f.suptitle(u"%s\n%s\n%s" % (RBC, HGB, HCT), fontsize=12, fontweight="bold")
     f.subplots_adjust(hspace=0.1)
@@ -155,8 +178,10 @@ def draw_second_part_of_erythrocytes(morphology, show_only):
     MCV = morphology.get_probe("MCV")
     MCH = morphology.get_probe("MCH")
 
-    draw(ax1, MCV.data_samples, MCV.data_limits, MCV.data_name, MCV.data_unit, morphology.dates)
-    draw(ax2, MCH.data_samples, MCH.data_limits, MCH.data_name, MCH.data_unit, morphology.dates)
+    for_women = morphology.show_data_limits_for_women()
+    
+    draw(ax1, MCV.get_samples(), MCV.get_limits(for_women), MCV.get_name(), MCV.get_unit(), morphology.dates)
+    draw(ax2, MCH.get_samples(), MCH.get_limits(for_women), MCH.get_name(), MCH.get_unit(), morphology.dates)
 
     f.suptitle(u"%s\n%s" % (MCV, MCH), fontsize=12, fontweight="bold")
     f.subplots_adjust(hspace=0.1)
@@ -173,8 +198,10 @@ def draw_third_part_of_erythrocytes(morphology, show_only):
     MCHC = morphology.get_probe("MCHC")
     RDW = morphology.get_probe("RDW")
 
-    draw(ax1, MCHC.data_samples, MCHC.data_limits, MCHC.data_name, MCHC.data_unit, morphology.dates)
-    draw(ax2, RDW.data_samples, RDW.data_limits, RDW.data_name, RDW.data_unit, morphology.dates)
+    for_women = morphology.show_data_limits_for_women()
+    
+    draw(ax1, MCHC.get_samples(), MCHC.get_limits(for_women), MCHC.get_name(), MCHC.get_unit(), morphology.dates)
+    draw(ax2, RDW.get_samples(), RDW.get_limits(for_women), RDW.get_name(), RDW.get_unit(), morphology.dates)
 
     f.suptitle(u"%s\n%s" % (MCHC, RDW), fontsize=12, fontweight="bold")
     f.subplots_adjust(hspace=0.1)
@@ -192,9 +219,11 @@ def draw_thrombocytes(morphology, show_only):
     PDW = morphology.get_probe("PDW")
     MPV = morphology.get_probe("MPV")
 
-    draw(ax1, PLT.data_samples, PLT.data_limits, PLT.data_name, PLT.data_unit, morphology.dates)
-    draw(ax2, PDW.data_samples, PDW.data_limits, PDW.data_name, PDW.data_unit, morphology.dates)
-    draw(ax3, MPV.data_samples, MPV.data_limits, MPV.data_name, MPV.data_unit, morphology.dates)
+    for_women = morphology.show_data_limits_for_women()
+    
+    draw(ax1, PLT.get_samples(), PLT.get_limits(for_women), PLT.get_name(), PLT.get_unit(), morphology.dates)
+    draw(ax2, PDW.get_samples(), PDW.get_limits(for_women), PDW.get_name(), PDW.get_unit(), morphology.dates)
+    draw(ax3, MPV.get_samples(), MPV.get_limits(for_women), MPV.get_name(), MPV.get_unit(), morphology.dates)
 
     f.suptitle(u"%s\n%s\n%s" % (PLT, PDW, MPV), fontsize=12, fontweight="bold")
     f.subplots_adjust(hspace=0.1)
@@ -211,8 +240,10 @@ def draw_leukocytes_and_plasma(morphology, show_only):
     WBC = morphology.get_probe("WBC")
     PCT = morphology.get_probe("PCT")
 
-    draw(ax1, WBC.data_samples, WBC.data_limits, WBC.data_name, WBC.data_unit, morphology.dates)
-    draw(ax2, PCT.data_samples, PCT.data_limits, PCT.data_name, PCT.data_unit, morphology.dates)
+    for_women = morphology.show_data_limits_for_women()
+    
+    draw(ax1, WBC.get_samples(), WBC.get_limits(for_women), WBC.get_name(), WBC.get_unit(), morphology.dates)
+    draw(ax2, PCT.get_samples(), PCT.get_limits(for_women), PCT.get_name(), PCT.get_unit(), morphology.dates)
 
     f.suptitle(u"%s\n%s" % (WBC, PCT), fontsize=12, fontweight="bold")
     f.subplots_adjust(hspace=0.1)
@@ -225,6 +256,7 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-f", type=str, required=False, help="Input data csv filename")
     parser.add_argument("-s", action="store_true", required=False, help="Show data without saving")
+    parser.add_argument("-k", action="store_true", required=False, help="Draw normal values for women instead of men")
     args = parser.parse_args()
 
     if args.f is not None:
@@ -233,8 +265,9 @@ if __name__ == "__main__":
         input_filename = "./data.csv"
 
     show_only = args.s
+    data_limits_for_women = args.k
 
-    morphology = Morphology()
+    morphology = Morphology(data_limits_for_women)
     morphology.read_data(input_filename)
     draw_first_part_of_erythrocytes(morphology, show_only)
     draw_second_part_of_erythrocytes(morphology, show_only)
